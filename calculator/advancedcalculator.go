0db3541c679f6cea0765ed6cbc03d3b3de1bc904
Rishi Raj <rishiraj88+github@gmail.com>
2024-12-15 19:58:47 +0100

    Test pushing with main branch.

--- calculator/advancedcalculator.go
+++ calculator/advancedcalculator.go
@@ -9,38 +9,44 @@ import (
 
 // Change these boolean values to control whether you see
 // the expected answer and/or hints.
-const showExpectedResult = false;
-const showHints = false;
+const showExpectedResult = false
+const showHints = false
 
-func main(){
-	value1 := "10"
-	value2 := "5.5"
+func main() {
+	value1 := "20"
+	value2 := "23232.5"
 	operation := "*"
-	result,status :=	calculate(value1, value2, operation)
+	result, status := calculate(value1, value2, operation)
 	fmt.Println(result)
 	if "" != status {
 		panic(status)
-	} 
+	}
 }
+
 // calculate() returns the result of the requested operation.
-func calculate(input1 string, input2 string, operation string) (float64,string) {
+func calculate(input1 string, input2 string, operation string) (float64, string) {
 	val1 := convertInputToValue(input1)
 	val2 := convertInputToValue(input2)
-	
+
 	switch operation {
-		case "+": return addValues(val1,val2),""
-		case "-": return subtractValues(val1,val2),""
-		case "*": return multiplyValues(val1,val2),""
-		case "/": return divideValues(val1,val2),""
-		default: return -99-99990909,"invalid arguments"
+	case "+":
+		return addValues(val1, val2), ""
+	case "-":
+		return subtractValues(val1, val2), ""
+	case "*":
+		return multiplyValues(val1, val2), ""
+	case "/":
+		return divideValues(val1, val2), ""
+	default:
+		return -99 - 99990909, "invalid arguments"
 	}
 }
 
 func convertInputToValue(input string) float64 {
-	val,err := strconv.ParseFloat(input,64)
+	val, err := strconv.ParseFloat(input, 64)
 	var errMsg string
-	if(nil != err) {
-		errMsg = fmt.Sprintf("Error while parsing an input. %v must be a number.",input)
+	if nil != err {
+		errMsg = fmt.Sprintf("Error while parsing an input. %v must be a number.", input)
 		panic(errMsg)
 	}
 	return val
@@ -62,4 +68,3 @@ func multiplyValues(value1, value2 float64) float64 {
 func divideValues(value1, value2 float64) float64 {
 	return value1 / value2
 }
-
